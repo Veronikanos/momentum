ccad6d25b84d6fc2f5faca6f5323ae9a9451d122
Veronica <vveronikanika@gmail.com>
2023-02-22 21:35:58 +0200

    fix: delete libraries, used fetch

--- momentum/src/weather.js
+++ momentum/src/weather.js
@@ -1,6 +1,3 @@
-import axios from 'axios';
-import {Notify} from 'notiflix/build/notiflix-notify-aio';
-
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector(
@@ -11,22 +8,27 @@ const humidity = document.querySelector('.humidity');
 const inputCity = document.querySelector('.city');
 const titleWeather = document.querySelector('.title-weather');
 
-const weatherService = axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5/weather',
-  params: {
-    q: 'Минск',
-    lang: 'en',
-    appid: '8ab18f9fbf76b2fee381039f2e960e34',
-    units: 'metric',
-  },
-});
+// const weatherService = axios.create({
+//   baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+//   params: {
+//     q: 'Минск',
+//     lang: 'en',
+//     appid: '8ab18f9fbf76b2fee381039f2e960e34',
+//     units: 'metric',
+//   },
+// });
 
 const getWeather = async (city = 'Минск') => {
   try {
-    const {data} = await weatherService.get('', {
-      params: {q: city},
-    });
-    return data;
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=8ab18f9fbf76b2fee381039f2e960e34&units=metric`
+    );
+    if (response.ok) {
+      let data = await response.json();
+      return data;
+    } else {
+      alert('Something went wrong');
+    }
   } catch (error) {
     return error.message;
   }
@@ -34,10 +36,7 @@ const getWeather = async (city = 'Минск') => {
 
 export const fillElementsOnWeatherBlock = async (city = 'Minsk') => {
   if (city === '') {
-    Notify.failure('Empty input', {
-      position: 'center-top',
-      timeout: 2000,
-    });
+    alert('Empty input');
     return;
   }
   try {
@@ -51,10 +50,7 @@ export const fillElementsOnWeatherBlock = async (city = 'Minsk') => {
     titleWeather.textContent =
       city.charAt(0).toUpperCase() + city.slice(1) ?? Minsk;
   } catch (error) {
-    Notify.failure('Such city doesn`t exist, try another one', {
-      position: 'center-top',
-      timeout: 2000,
-    });
+    alert('Such city doesn`t exist, try another one');
   }
 };
 
