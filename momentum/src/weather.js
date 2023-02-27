1b63cf91cdce4b8f1104aefa43e98c83f5f85fff
Veronica <vveronikanika@gmail.com>
2023-02-27 15:59:12 +0200

    refactor: set default city if no arguments

--- momentum/src/weather.js
+++ momentum/src/weather.js
@@ -9,7 +9,7 @@ const inputCity = document.querySelector('.city');
 const titleWeather = document.querySelector('.title-weather');
 const updatedInfoBox = document.querySelector('.updated-info');
 
-const getWeather = async (city = 'Минск') => {
+const getWeather = async (city) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=8ab18f9fbf76b2fee381039f2e960e34&units=metric`
   );
@@ -21,13 +21,9 @@ const getWeather = async (city = 'Минск') => {
   }
 };
 
-export const fillElementsOnWeatherBlock = async (city = 'Minsk') => {
-  if (city === '') {
-    alert('Empty input');
-    return;
-  }
+export const fillElementsOnWeatherBlock = async (city) => {
   try {
-    const {weather, main, wind} = await getWeather(city);
+    const {weather, main, wind} = await getWeather(city || 'Minsk');
 
     weatherIcon.classList.add(`owf-${weather[0].id}`);
     temperature.textContent = `${Math.round(main.temp)}°C`;
@@ -43,8 +39,12 @@ export const fillElementsOnWeatherBlock = async (city = 'Minsk') => {
 };
 
 const searchCity = () => {
+  if (!inputCity.value.toLowerCase().trim()) {
+    alert('Empty input');
+    return;
+  }
   fillElementsOnWeatherBlock(inputCity.value.toLowerCase().trim());
 };
 
-fillElementsOnWeatherBlock();
+fillElementsOnWeatherBlock(localStorage.getItem('city') || 'Minsk');
 inputCity.addEventListener('change', searchCity);
